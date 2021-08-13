0e1ade54890afd7471b69e66ed5ba63b2595fbe0
Usyk <milausik@ukr.net>
2021-08-13 18:54:05 +0300

    color-switch

--- index.js
+++ index.js
@@ -1,18 +1,33 @@
 const colors = [
-    '#FFFFFF',
-    '#2196F3',
-    '#4CAF50',
-    '#FF9800',
-    '#009688',
-    '#795548',
-  ];
+  "#FFFFFF",
+  "#2196F3",
+  "#4CAF50",
+  "#FF9800",
+  "#009688",
+  "#795548",
+];
+const randomIntegerFromInterval = (min, max) => {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
 
-  const randomIntegerFromInterval = (min, max) => {
-    return Math.floor(Math.random() * (max - min + 1) + min);
-  };
+const startBtn = document.querySelector('[data-action="start"]');
+const stopBtn = document.querySelector('[data-action="stop"]');
+const body = document.querySelector("body");
 
-  const start = document.querySelector('[data-action="start"]');
-  const stop = document.querySelector('[data-action="stop]');
-  const body = document.querySelector("body");
+function colorSwitcher() {
+  let selectedColor = colors[`${randomIntegerFromInterval(0, colors.length)}`];
+  body.style.backgroundColor = selectedColor;
+  startBtn.setAttribute("disabled", "disabled");
+}
 
-  
+let intervalSwitcher = null;
+
+startBtn.addEventListener("click", () => {
+  intervalSwitcher = setInterval(() => {
+    colorSwitcher();
+  }, 1000);
+});
+
+stopBtn.addEventListener("click", () => {
+  clearInterval(intervalSwitcher), startBtn.removeAttribute("disabled");
+});
